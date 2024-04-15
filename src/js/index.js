9c2d56896e59467c26aa067d0f203fb9ab70b51d
Kirill Poltorak <computer45456@gmail.com>
2024-04-15 22:21:27 +0300

    test #1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,23 @@
 import * as $ from 'jquery';
 import mobileNav from './modules/mobile-nav.js';
 import tabs from './modules/tabs.js';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
 mobileNav();
 tabs();
+
+const swiper = new Swiper('.swiper', {
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
